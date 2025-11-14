897624c227afa6374f554eaebd5f2ae3d430fb6a
Yess <ycardenas@fitpal.co>
2025-11-14 01:09:49 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todayTask',
+  webDir: 'www'
+};
+
+export default config;
